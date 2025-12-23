2713628a6c683bcaba04ba87223583f25aa93289
Simon Hryszko <simonhryszko@gmail.com>
2025-12-23 17:44:37 +0700

    🎵 waybar: scroll to seek ±5 seconds

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -85,8 +85,8 @@ in
             on-click = "${pkgs.bluez}/bin/bluetoothctl connect ${btHeadphonesMac}";
             on-click-right = "${pkgs.bluez}/bin/bluetoothctl disconnect ${btHeadphonesMac}";
             on-click-middle = "${pkgs.playerctl}/bin/playerctl play-pause";
-            on-scroll-up = "${pkgs.playerctl}/bin/playerctl next";
-            on-scroll-down = "${pkgs.playerctl}/bin/playerctl previous";
+            on-scroll-up = "${pkgs.playerctl}/bin/playerctl position 5+";
+            on-scroll-down = "${pkgs.playerctl}/bin/playerctl position 5-";
           };
 
           "network" = {
