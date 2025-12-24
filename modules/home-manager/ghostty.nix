500fc90d4b295d3ea0b16e688cf953e406d559cd
Simon Hryszko <simonhryszko@gmail.com>
2025-12-24 13:06:11 +0700

    🔧 terminal: add ghostty settings and change theme

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -9,8 +9,26 @@
     programs.ghostty = {
       enable = true;
       settings = {
-        theme = "Blue Matrix";
+        theme = "Matrix";
+        background-opacity = 0.95;
+        background-blur-radius = 25;
+        font-size = 12;
+        cursor-style = "block";
+        cursor-style-blink = true;
+        cursor-invert-fg-bg = true;
+        mouse-hide-while-typing = true;
+        confirm-close-surface = false;
+        window-decoration = true;
+        window-padding-x = 10;
+        window-padding-y = 10;
+        window-padding-balance = true;
+        shell-integration-features = "no-cursor";
         copy-on-select = "clipboard";
+        keybind = [
+          "ctrl+shift+p=unbind"
+          "ctrl+shift+n=unbind"
+          "f1=toggle_command_palette"
+        ];
       };
     };
   };
